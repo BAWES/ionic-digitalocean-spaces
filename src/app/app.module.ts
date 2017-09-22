eae3a78718bfa5c8ee54911f1e292c4bd9eeccc8
Khalid <khalid@bawes.net>
2017-09-22 09:59:32 +0300

    Setup base files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,14 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { Camera } from '@ionic-native/camera';
+import { File } from '@ionic-native/file';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
+import { AwsService } from '../providers/aws.service';
+import { CameraService } from '../providers/camera.service';
 
 @NgModule({
   declarations: [
@@ -22,8 +27,12 @@ import { HomePage } from '../pages/home/home';
     HomePage
   ],
   providers: [
+    AwsService,
+    CameraService,
     StatusBar,
     SplashScreen,
+    Camera,
+    File,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
